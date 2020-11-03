95caeb2da604bd4cf169d6b031fbf2433afac340
Alex <47815470+TheB1ack@users.noreply.github.com>
2020-11-03 18:04:48 +0200

    Background effect added

---
+++ MapNotepad/MapNotepad/MapNotepad.Android/Effects/BackgroundAndriodEffect.cs
@@ -0,0 +1,26 @@
+﻿using Android.Graphics.Drawables;
+using MapNotepad.Droid.Effects;
+using MapNotepad.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(BackgroundAndriodEffect), nameof(BackgroundEffect))]
+namespace MapNotepad.Droid.Effects
+{
+    public class BackgroundAndriodEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Rgb(41, 214, 149));
+            }
+        }
+
+        protected override void OnDetached()
+        {
+
+        }
+    }
+}
---
+++ MapNotepad/MapNotepad/MapNotepad/Effects/BackgroundEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MapNotepad.Effects
+{
+    public class BackgroundEffect: RoutingEffect
+    {
+        public BackgroundEffect() : base($"MyCompany.{nameof(BackgroundEffect)}")
+        {
+        }
+    }
+}
