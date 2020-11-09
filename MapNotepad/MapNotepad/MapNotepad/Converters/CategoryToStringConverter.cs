c27b930c9939a9d8a67556e541ac42246556368d
Alex <47815470+TheB1ack@users.noreply.github.com>
2020-11-09 18:27:44 +0200

    New list cell design

---
+++ MapNotepad/MapNotepad/MapNotepad/Converters/CategoryToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using MapNotepad.Enums;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MapNotepad.Converters
+{
+    public class CategoryToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((SearchCategories)(int)value).ToString();
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)(SearchCategories)value;
+        }
+    }
+}
